433874767af0344acf8c16b7a4c9c00e7010c999
Viclo <ivangra85@gmail.com>
2021-10-23 10:09:20 -0400

    Recupera datos de la BD con consultas query desde routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../config/database');
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get('/', async (req, res) => {
+    const cursos = await pool.query('SELECT * FROM CURSO');
+    console.log(cursos);
+    res.redirect('index');
 });
 
 module.exports = router;
