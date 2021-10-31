c2aec375bb62f1d37d71417b91cd7895bbd3cab8
maurihv13 <huaytamauricio1@gmail.com>
2021-10-31 08:31:57 -0400

    Agregando tipo de letra

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
 
     database: {
         host: 'localhost',
-        port: '3307',
+        port: '3306',
         user: 'root',
         password: '',
         database: 'practicadb'
