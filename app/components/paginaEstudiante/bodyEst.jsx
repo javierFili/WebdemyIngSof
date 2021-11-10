33c6dd2e6895fd3c9b24c9aef0f998a67b428b01
maurihv13 <huaytamauricio1@gmail.com>
2021-11-10 12:17:21 -0400

    Avance en vista de cartas de curso

--- app/components/paginaEstudiante/bodyEst.jsx
+++ app/components/paginaEstudiante/bodyEst.jsx
@@ -1,6 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class VistaEst extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            usuario: props.id_usuario,
+            cursos: []
+        }
+        this.fetchCourse = this.fetchCourse.bind(this);
+        this.cortar = this.cortar.bind(this);
+        this.refrescarPagina = this.refrescarPagina.bind(this);
+    
+        this.nombreAbuscar = "";
+    }
+
+    componentDidMount(){
+        this.fetchCourse();
+    }
+
+    fetchCourse(){
+        fetch('/api/cursos/cursos') //Se cargan los datos de todos los cursos, para pruebas
+            .then(res => res.json())
+            .then(data => {
+                this.setState({cursos: data});
+            });
+    }
+
+    cortar(obj){
+        let text = JSON.stringify(obj);
+        return text.slice(1,11);
+    }
+
+    sacar() {
+    
+        var filtracion0= this.state.cursos.filter(curso =>curso.nombreEtiqueta.includes(this.nombreAbuscar) ||
+        this.nombreAbuscar === "" );
+    
+        let hash = {};
+        var filtracion = filtracion0.filter(curso=>hash[curso.id_curso]? false : hash[curso.id_curso]=true);
+        return filtracion;
+    }
+
+    refrescarPagina(){
+        window.location.href = window.location.href;
+    }
+
     render() {
         return(
             <div className="fondoEst">
@@ -10,15 +55,33 @@ class VistaEst extends Component{
                 </div>
                 <p> Mis cursos: </p>
                 <div className="contCursosEst">
-                    <div>
-                        <p> Curso1 </p>
-                    </div>
-                    <div>
-                        <p> Curso2 </p>
-                    </div>
-                    <div>
-                        <p> Curso3 </p>
-                    </div>
+                    {
+                        this.sacar().map(curso => {
+                            return(
+                            <div>
+                                <button className="elementos-carrusel" onClick={this.refrescarPagina}>
+                                    <Link className='linkInial' to={`/Inicio/${curso.id_curso}`} > 
+                                        <div key={curso.id_curso} className="linkCursoEst">
+                                            <div>
+                                                <img id="imagenCursoRed" src={`${process.env.PUBLIC_URL}/assets/imagenes/${curso.imagen}`}></img>
+                                            </div>
+                                            <div>
+                                                <h3>{curso.nombreCurso}</h3>                  
+                                                <div className="curseDescription">
+                                                    Actualizacion: {this.cortar(curso.created_at) }
+                                                    <br />
+                                                    Inscritos: { curso.inscritos }
+                                                    <br />
+                                                    Tutor: {curso.nomT} {curso.apellT}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link> 
+                                </button>
+                            </div>                  
+                            )
+                            })
+                    }
                 </div>
             </div>
         )
